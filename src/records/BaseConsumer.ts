0eff0da2f07cd0289ad5a31fdaf789be6cd5a8f0
Sangwoo <swp@laonpeople.com>
2020-12-17 10:23:58 +0900

    Create BaseConsumer class

---
+++ src/records/BaseConsumer.ts
@@ -0,0 +1,52 @@
+import { Kafka, Consumer, KafkaMessage } from 'kafkajs';
+
+import { IBaseRecord } from './BaseRecord';
+
+/**
+ * Base Kafka consumer.
+ */
+export abstract class BaseConsumer<T extends IBaseRecord> {
+    abstract topic: string;
+    abstract consumerGroup: string;
+    abstract eachMessage(key: T['key'], value: T['value']): void;
+
+    private kafka: Kafka;
+    private consumer!: Consumer
+
+    /**
+     * Inject kafka.
+     *
+     * @param kafka Kafka object.
+     */
+    constructor(kafka: Kafka) {
+        this.kafka = kafka;
+    }
+
+    /**
+     * Subscribe the topic within a given consumer group.
+     */
+    async subscribe() {
+        this.consumer = this.kafka.consumer({ groupId: this.consumerGroup });
+
+        await this.consumer.connect();
+
+        await this.consumer.subscribe({
+            topic: this.topic,
+            fromBeginning: true
+        });
+
+        await this.consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                const parsedValue = this.parseValue(message);
+
+                this.eachMessage(message.key?.toString() || "", parsedValue);
+            }
+        });
+    }
+
+    private parseValue(message: KafkaMessage) {
+        const value = message.value?.toString();
+
+        return value ? JSON.parse(value) : value;
+    }
+}
